905144c190dd7518e6af2935eea52f06f18e7913
KarimEttayi <karim.ettayi@polymtl.ca>
2020-09-15 23:19:21 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     
-    return string[0:5] + "w" 
+    return string[0:5] + "w" + string[]
 
 
 def get_number_of_char(string: str, char: str) -> int:
