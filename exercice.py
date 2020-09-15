3b7c7a286aec584f269a4eb19140d74bdece9028
KarimEttayi <karim.ettayi@polymtl.ca>
2020-09-15 23:36:52 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,8 +19,8 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    string.count()
-    return X
+    string.count(char)
+    return str
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
